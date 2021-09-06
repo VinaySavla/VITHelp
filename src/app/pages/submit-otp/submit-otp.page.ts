542c73f748df858b0ab1c4efffb9f873694bf4f0
MuursaleenBats <mursaleenbatatawala@gmail.com>
2021-09-06 13:15:46 +0530

    fetched data from volunteer and distress table

--- src/app/pages/submit-otp/submit-otp.page.ts
+++ src/app/pages/submit-otp/submit-otp.page.ts
@@ -4,7 +4,7 @@ import { FormBuilder, Validators } from "@angular/forms";
 import { CrudService } from "src/app/service/crud.service";
 import { CommonPopoverService } from "src/app/providers/common-popover/common-popover.service";
 import { NetworkConnectionService } from "src/app/providers/network-connection/network-connection.service";
-import { HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { StorageProvider } from "src/app/providers/storage/storage.service";
 
 @Component({
@@ -17,13 +17,14 @@ export class SubmitOtpPage implements OnInit {
   otp: number;
   timer: number = 30;
   phnNo: any;
-  array: any=[];
+  array: any = [];
+  array1: any = [];
   time = setInterval(() => {
     this.timer -= 1;
   }, 1000);
   otpForm: any;
   constructor(
-    private crudService: CrudService, 
+    private crudService: CrudService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
     private router: Router,
@@ -31,13 +32,13 @@ export class SubmitOtpPage implements OnInit {
     private commonPopover: CommonPopoverService,
     private networkConnection: NetworkConnectionService,
     private keystore: StorageProvider
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.otpForm = this.formBuilder.group({
       otp: ["", [Validators.required, Validators.pattern(/^[0-9]{4,4}$/)]]
     });
-    
+
   }
   changeOTP(value) {
     if (value) {
@@ -69,27 +70,36 @@ export class SubmitOtpPage implements OnInit {
       phone: this.route.snapshot.paramMap.get("phone"),
       otp: parseInt(this.otpForm.controls["otp"].value)
     };
-    
+
     await this.commonPopover.loaderPresent("Verifying OTP");
     //TODO Verify OTP Method
     //TODO if otp verified : this.keystore.set("isAuthenticated", true); to be added
-    this.phnNo =this.keystore.get('phnNo');
-    this.getPhnno();
-    for (var i=0; i<=this.array.length; i++) {
+    this.phnNo = this.keystore.get('phnNo');
+    this.getVoluntPhn();
+    this.getDistressPhn();
+    for (var i = 0; i <= this.array.length; i++) {
       if (this.array[i] === this.phnNo) {
-      alert("present");
+        alert("Number registered as volunteer");
+      }
+      else {
+        alert("not present");
+      }
     }
-    else {
-      alert("not present");
+    for (var i = 0; i <= this.array1.length; i++) {
+      if (this.array1[i] === this.phnNo) {
+        alert("Number registered as distressed");
+      }
+      else {
+        alert("not present");
+      }
     }
-  } 
-  console.log(JSON.stringify(this.array));
+    console.log(JSON.stringify(this.array));
     this.keystore.set("isAuthenticated", true);
     this.commonPopover.loaderDismiss();
     this.router.navigate(["/select-role"]);
   }
-  getPhnno() {
-    this.http.get<any>('http://covithelp.16mb.com/phnno.php').subscribe(data => {
+  getVoluntPhn() {
+    this.http.get<any>('http://covithelp.16mb.com/voluntphn.php').subscribe(data => {
       //var response = data;
       this.array = data.split(" ")
       this.array = this.array.map((str) => Number(str))
@@ -99,7 +109,17 @@ export class SubmitOtpPage implements OnInit {
       console.log(error)
     })
   }
-  
+  getDistressPhn() {
+    this.http.get<any>('http://covithelp.16mb.com/distressphn.php').subscribe(data => {
+      //var response = data;
+      this.array1 = data.split(" ")
+      this.array1 = this.array1.map((str) => Number(str))
+      // this.array = data;
+      console.log(this.array1);
+    }, error => {
+      console.log(error)
+    })
+  }
 
   ionViewWillLeave() {
     clearInterval(this.time);
