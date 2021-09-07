82c558a9cc9f30f2ba3f24adad636506263ba8d1
HARSHAL MENGE <meharshal1703@gmail.com>
2021-09-07 12:24:06 +0530

    Chat pages

--- src/app/pages/home/notification/case/case.page.ts
+++ src/app/pages/home/notification/case/case.page.ts
@@ -6,17 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./case.page.scss'],
 })
 export class CasePage implements OnInit {
-title="case: "
-isHidden:boolean=true;
+  title = "case: "
+  isHidden: boolean = true;
+  contentHidden: boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
-  addSupport(){
-    this.isHidden=false;
+  addSupport() {
+    this.isHidden = false;
   }
-  sendSupport(){
-    this.isHidden=true;
+  sendSupport() {
+    this.isHidden = true;
+  }
+  showContent() {
+    if (this.contentHidden) {
+      this.contentHidden = false;
+    }
+    else {
+      this.contentHidden = true;
+    }
   }
 
+  // hideContent(){
+  //   this.contentHidden=true;
+  // }
+
+  messages = [
+    {
+      user: 'useer1',
+      createdAt: 1554090856000,
+      msg: 'Attending'
+    },
+    {
+      user: 'useer2',
+      createdAt: 1554090856000,
+      msg: '',
+    },
+    {
+      user: 'useer3',
+      createdAt: 1554090856000,
+      msg: '',
+    },
+    {
+      user: 'useer4',
+      createdAt: 1554090856000,
+      msg: '',
+    }
+  ];
+
+
+
+  sendessage() { }
+
 }
+
+// export class CasePage{
+
+// }
+
