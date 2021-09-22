64e11e8c1c04d54e07258148a5ba6c4d168d7722
anurag-393 <anuragd393@gmail.com>
2021-09-22 15:17:03 +0530

    updated cb

--- src/app/pages/home/distressed/chatbot/chatbot.page.ts
+++ src/app/pages/home/distressed/chatbot/chatbot.page.ts
@@ -18,12 +18,12 @@ export class ChatbotPage implements OnInit {
 
     const script2 = document.createElement('script');
     script2.type = 'text/javascript';
-    script2.src = "/assets/build/static/js/2.dea57047.chunk.js";
+    script2.src = "/assets/build/static/js/2.3ec46d55.chunk.js";
     document.getElementsByTagName("body")[0].appendChild(script2);
 
     const script3 = document.createElement('script');
     script3.type = 'text/javascript';
-    script3.src = "/assets/build/static/js/main.3566c3d7.chunk.js";
+    script3.src = "/assets/build/static/js/main.b41bf761.chunk.js";
     document.getElementsByTagName("body")[0].appendChild(script3);
 
     // TODO
